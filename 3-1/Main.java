21a2af2eef56a43c9c342328d4e2917865d56902
kira-tomomi <102976535+kira-tomomi@users.noreply.github.com>
2022-04-21 10:57:19 +0900

    課題提出テスト
    ３－１課題提出テスト

---
+++ 3-1/Main.java
@@ -0,0 +1,35 @@
+
+/**
+ * バブルソート
+ * チェックテスト-Java３章
+ *
+ */
+public class Main{
+  public static void main(String[] args){
+    int[] data = {3,1,2,7,5};
+      /*
+      * 問2
+      * 以下のfor文を完成させなさい
+      */
+      for(int i = 0; i < data.length ; i++){
+          System.out.print(data[i] + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < data.length-2; i++) {
+            for (int j = data.length-1; j > i; j--) {
+                /*
+                * 問3
+                * 以下、配列の添字を入れてソートを完成させなさい
+                */
+                if(data[j-1] > data[j]){
+                  int box = data[j-1];
+                  data[j-1] = data[j];
+                  data[j] = box;
+                }
+            }
+        }
+        for(int i = 0; i < data.length; i++){
+            System.out.print(data[i] + " ");
+        }
+    }
+}
